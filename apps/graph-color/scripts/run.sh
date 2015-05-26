03a17501129ec29e9d936b7fb4109f83e08f7aa9
Da Li <daniel.lee.bit@gmail.com>
2015-05-25 23:44:25 -0400

    + Add r.sh for BC and delete graph-color run.sh

--- apps/graph-color/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./bfs 0 0 0 < ../../data/bfs/USA-road-d.COL.gr | tee -a July-02.txt
-./bfs 1 0 0 < ../../data/bfs/coPapersCiteseer.graph | tee -a July-02.txt
-./bfs 2 0 0 < ../../data/bfs/p2p-Gnutella31.txt | tee -a July-02.txt
-./bfs 2 0 0 < ../../data/bfs/amazon0505.txt | tee -a July-02.txt
-./bfs 2 0 0 < ../../data/bfs/web-Google.txt | tee -a July-02.txt
-./bfs 2 0 0 < ../../data/bfs/soc-LiveJournal1.txt | tee -a July-02.txt
