cff6b361ca99c2154a8ec74cc7bce3a1232e2fb5
Da Li <daniel.lee.bit@gmail.com>
2015-05-26 23:12:19 -0400

    + Add test.sh for graph-color

--- apps/graph-color/scripts/test.sh
+++ apps/graph-color/scripts/test.sh
@@ -1,9 +1,8 @@
-#./gpu-ir-synth -f 0 -i data_test.gr -v 
-#./gpu-ir-synth -f 0 -i data_test.gr -v -d
-#./gpu-ir-synth -f 2 -i ../../../data/p2p-Gnutella05.txt -v -d -s 1
-./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 2
-./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 3
-./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 4
-./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 5
-./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 6
+#!/bin/bash
 
+BIN=../src/gpu/gpu-graph-color
+DATA_FILE=../../../datasets/DIMACS10/coPapersCiteseer.graph
+LOG_FILE=sp_performance.log
+
+EXE=$BIN
+$EXE -f 1 -i ${DATA_FILE} -v -s 7
